c42c1affca8d6b15cba66785550bb814e6727af8
flynn <flynn@dead.place>
2022-11-08 14:25:14 +0000

    style: match Twitter's styling somewhat on Tweet.tsx

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -12,12 +12,40 @@ interface Props {
 
 const Tweet = ({ tweet, ...props }: Props) => {
   return (
-    <div className="flex h-full w-full flex-col bg-white">
-      <h1 className="text-xl">{props.user?.name}</h1>
-      <h1 className="pb-8">@{props.user?.username}</h1>
-
-      <h1>{tweet.text}</h1>
-      <h1>{tweet.createdAt.toLocaleString()}</h1>
+    <div className="flex h-full w-full flex-col bg-white p-4">
+      <div
+        className={"flex h-full flex-row gap-2 " + (props.expanded ? "" : "")}
+      >
+        <div className="flex pt-1">
+          <img
+            className="h-12 w-12 rounded-md"
+            src={props.user?.image as string}
+          ></img>
+        </div>
+        <div className={"flex flex-col pl-1"}>
+          <div
+            className={
+              "flex " + (props.expanded ? "flex-col" : "flex-row items-center")
+            }
+          >
+            <h1
+              className={"text-base " + (props.expanded ? "text-xl" : "pr-1")}
+            >
+              {props.user?.name}
+            </h1>
+            <h1
+              className={
+                "text-base font-light text-[#9ca8b2] " +
+                (props.expanded ? "" : "")
+              }
+            >
+              @{props.user?.username}
+            </h1>
+          </div>
+          <p className="pb-2 pt-1">{tweet.text}</p>
+          <p className="text-sm">{tweet.createdAt.toLocaleString()}</p>
+        </div>
+      </div>
     </div>
   );
 };
